fd8365d3493b70ff5db55d12be4bc44a6d3bc6ef
hp\Administrator <912072089@qq.com>
2018-06-19 20:02:18 +0800

    * [vue-learn] 带搜索的选择组件

---
+++ src/components/select-search.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="search-select">
+    <div class="search-area">
+      <input class="input" type="text" @input="changeValue($event)">
+      <ul class="items">
+        <li class="item" v-for="(item, index) in items" :key="index">{{item.label}}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      items: [...this.list]
+    }
+  },
+  props: {
+    list: {
+      type: Array,
+      default () {
+        return []
+      }
+    }
+  },
+  methods: {
+    changeValue (event) {
+      const val = event.target.value
+      const newArr = []
+      this.items.filter(({label, value}) => {
+        if (label.indexOf(val) >= 0) {
+          newArr.unshift({label, value})
+        }
+      })
+      this.items = [...newArr]
+      if (this.items.length === 0 || val === '') {
+        this.items = [...this.list]
+      }
+    },
+    watchList () {
+      console.log('----->')
+      this.items = [...this.list]
+    }
+  },
+  mounted () {
+    this.$watch('list', this.watchList)
+  }
+}
+</script>
+
+<style scoped>
+.search-select {
+  position: relative;
+  font-size: 0.28rem;
+}
+
+.search-select .input-wrap {
+  height: .34rem;
+  line-height: .34rem;
+}
+
+.search-select .input-wrap .label {
+  height: .34rem;
+  line-height: .34rem;
+  color: #393939;
+  background: none !important;
+}
+
+.search-select .input {
+  width: 100%;
+}
+
+.search-select .search-area {
+  position: relative;
+  padding-left: 0;
+  padding-right: 0;
+}
+
+.search-select .items {
+  position: absolute;
+  z-index: 1;
+  background-color: #fff;
+  width: 100%;
+  max-height: 1.5rem;
+  overflow-y: scroll;
+  margin-left: 0;
+  border: 1px solid #c4c4c4;
+}
+
+.search-select .item {
+  height: .3rem;
+  line-height: .3rem;
+  padding-left: .2rem;
+}
+
+.search-select .item:hover,
+.search-select .item .active {
+  background-color: #0000ff;
+}
+</style>
+
--- src/views/demo.vue
+++ src/views/demo.vue
@@ -3,15 +3,39 @@
     <confirm-dialogue :is-touch-close='isTouchClose'>
     </confirm-dialogue>
     <button @click="myMethods">显示确认组件</button>
+    <select-search :list="list"></select-search>
   </div>
 </template>
 
 <script>
   import ConfirmDialogue from '../components/confirmdialogue.vue'
+  import SelectSearch from '../components/select-search.vue'
   export default {
     data () {
       return {
-        isTouchClose: true
+        isTouchClose: true,
+        list: [
+          {
+            label: '工商银行',
+            value: '01'
+          },
+          {
+            label: '建设银行',
+            value: '02'
+          },
+          {
+            label: '农业银行',
+            value: '03'
+          },
+          {
+            label: '招商银行',
+            value: '04'
+          },
+          {
+            label: '交通银行',
+            value: '05'
+          }
+        ]
       }
     },
     methods: {
@@ -23,7 +47,8 @@
       }
     },
     components: {
-      ConfirmDialogue
+      ConfirmDialogue,
+      SelectSearch
     },
     mounted () {
       this.eventBus.$on('confirm/ok', this.test)
