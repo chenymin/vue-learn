79b289f7bbe0a88b8e2c81b58a67b7ee7d8ce065
陈阳民 <chenym1@fosun.com>
2019-06-13 17:49:04 +0800

    *[vue-h5] hook 代码提交

--- src/views/hook/hookDemo.vue
+++ src/views/hook/hookDemo.vue
@@ -1,5 +1,15 @@
 <template>
-  <ContactForm/>
+  <form @submit.prevent="submit">
+    <label class="contact-form-label">
+      Name
+      <input v-model="data.name">
+    </label>
+    <label class="contact-form-label">
+      Message
+      <textarea v-model="data.message"/>
+    </label>
+    <button>Submit</button>
+  </form>
 </template>
 
 <script>
@@ -8,17 +18,25 @@
   import { post } from '@/api/loanApply'
   import { withHooks } from '@/utils/hooks'
 
-  import ContactForm from './ContactForm.vue'
-
   export default {
-    components: {
-      ContactForm
-    },
     provide: {
       // We pass an additional `id` context
       // property to make it easier to identify
       // calls of `post()` when running our Hooks.
       post: withHooks(post, { id: 'contact-form.post' })
+    },
+    data () {
+      return { data: { name: '', message: '' } }
+    },
+    methods: {
+      submit () {
+        withHooks(this.test, { id: 'contact-form.post' })(this.data)
+      },
+      async test (param) {
+        const {message, name} = await post(param)
+        console.log(message, name)
+        return param
+      }
     }
   }
 </script>
